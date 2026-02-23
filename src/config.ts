a124040d592947222eefee16d688c0c78e77735e
Mic <tt402@outlook.com>
2026-02-23 15:19:24 +0800

    update: Footer

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: 'Mic.Run',
-	subtitle: 'Mic的博客',
+	subtitle: '麦克风.马上跑路',
 	lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
 	themeColor: {
 		hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -27,15 +27,15 @@ export const siteConfig: SiteConfig = {
 	},
 	toc: {
 		enable: true, // Display the table of contents on the right side of the post
-		depth: 2, // Maximum heading depth to show in the table, from 1 to 3
+		depth: 3, // Maximum heading depth to show in the table, from 1 to 3
 	},
 	favicon: [
 		// Leave this array empty to use the default favicon
-		// {
-		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-		//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-		// }
+		{
+		  src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
+		  // theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+		  // sizes: '64x64',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+		}
 	],
 };
 
@@ -53,9 +53,9 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: 'https://img.miku.ren/i/2025/03/09/67cd586c79326.jpg',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: 'https://q.qlogo.cn/headimg_dl?dst_uin=3098880154&spec=640&img_type=jpg',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: 'Mic',
-	bio: '关关难过关关过，前路灿灿亦漫漫。',
+	bio: '毕竟几人真得鹿，不知终日梦为鱼。',
 	links: [
 		{
 			name: 'BiliBili',
